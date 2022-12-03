bc8a675971e4eafb27ccfdbedf704ec167b785c1
Mahinthan200526620 <114129956+Mahinthan200526620@users.noreply.github.com>
2022-12-02 23:09:11 -0500

    first commit

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string? OwnerID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Address { get; set; }
+        [Required]
+        public string? City { get; set; }
+        [Required]
+        public string? State { get; set; }
+        [Required]
+        public string? Zip { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        public ContactStatus Status { get; set; }
+    }
+
+    public enum ContactStatus
+    {
+        Submitted,
+        Approved,
+        Rejected
+    }
+}
